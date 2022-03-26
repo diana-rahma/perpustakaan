de548a2025978682e52185f916be3956c12b1d82
diana-rahma <dianarhm12@gmail.com>
2022-03-26 11:38:57 +0700

    kategori migrasi

--- database/migrations/2022_03_26_025730_create_kategoris_table.php
+++ database/migrations/2022_03_26_025730_create_kategoris_table.php
@@ -15,6 +15,8 @@ class CreateKategorisTable extends Migration
     {
         Schema::create('kategoris', function (Blueprint $table) {
             $table->id();
+            $table->string('kategori');
+            $table->string('file');
             $table->timestamps();
         });
     }
