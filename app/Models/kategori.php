80a20117085768aeedb27c3e6db7028b4dcb1b1a
mwilians <98369617+mwilians@users.noreply.github.com>
2022-03-26 11:40:29 +0700

    update kategori.php

--- app/Models/kategori.php
+++ app/Models/kategori.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class kategori extends Model
 {
     use HasFactory;
+
+    protected $table = "kategoris";
+    protected $primarykey = "id";
+    protected $fillable = ['id','kategori','file'];
 }
